3c5d786b1e5832fd8fde7580590d1155cdc25145
coc00r1co <118786589+coc00r1co@users.noreply.github.com>
2023-05-23 10:27:20 -0400

    jquery interaction ..

---
+++ js/index.js
@@ -0,0 +1,54 @@
+$(".item-1").on( "mouseenter", function() {
+    $(".item-1").fadeIn();
+  });
+
+  $(".item-1").on( "mouseleave", function() {
+    $(".tools-wrapper").fadeOut();
+  });
+
+  $(".item-2").on( "mouseenter", function() {
+    $(".item-2").fadeIn();
+  });
+
+  $(".item-2").on( "mouseleave", function() {
+    $(".tools-wrapper").fadeOut();
+  });
+
+  $(".item-3").on( "mouseenter", function() {
+    $(".item-3").fadeIn();
+  });
+
+  $(".item-3").on( "mouseleave", function() {
+    $(".tools-wrapper").fadeOut();
+  });
+
+  $(".item-4").on( "mouseenter", function() {
+    $(".item-4").fadeIn();
+  });
+
+  $(".item-4").on( "mouseleave", function() {
+    $(".tools-wrapper").fadeOut();
+  });
+
+  // $(".skill-wrap item-1").on( "mouseleave", function() {
+  //   $(.this).fadeOut();
+  // });
+
+// function appendText() {
+//     var txt1 = "<p>Text.</p>";        // Create text with HTML
+//     var txt2 = $("<p></p>").text("Text.");  // Create text with jQuery
+//     var txt3 = document.createElement("p");
+//     txt3.innerHTML = "Text.";         // Create text with DOM
+//     $(".tools").append(txt1, txt2, txt3);   // Append new elements
+//   }
+
+// $(".skill-wrap").on( "mouseenter", function() {
+//     $(".tools").appendText();
+//   });
+
+
+  
+                      
+  console.log("heyhey");
+  
+  
